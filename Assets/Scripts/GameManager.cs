9aba82e769b5a650a6aba1ffe4ead24b0aff2788
Tanvir Ahmed <42252274+tan840@users.noreply.github.com>
2021-05-18 17:18:01 +0600

    GameManager Complete

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     public float spawnDelay = 0.5f;
     private float nextSpawn = 0f;
 
-
+    public bool paused;
     public GameObject cubePrefab;
     public Transform spawnPoint;
 
@@ -33,6 +33,18 @@ public class GameManager : MonoBehaviour
         {
             nextSpawn = Time.time + spawnDelay;
             Instantiate(cubePrefab,spawnPoint.position,spawnPoint.rotation);
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            paused = !paused;
+            if (paused)
+            {
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+            }
         }
     }
 }
